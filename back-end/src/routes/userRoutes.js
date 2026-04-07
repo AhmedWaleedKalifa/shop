7af42cb9ffe1aa866e3921aea36cd79ef731c12d
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2026-04-07 22:12:18 +0200

    s

--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -6,6 +6,7 @@ const { authenticate,authorize } = require('../middleware/auth');
 
 userRouter.get("/",authenticate,authorize("ADMIN","MODERATOR"), userController.getUsers);
 userRouter.get("/:id",authenticate,authorize("ADMIN","MODERATOR"), userController.getUserById);
+
 userRouter.put("/:id",authenticate,authorize("ADMIN"), userController.updateUser);
 userRouter.delete("/:id",authenticate,authorize("ADMIN"), userController.deleteUser);
 module.exports = userRouter;
